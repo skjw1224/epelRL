b40a63e301f231dac280a5731c86e8158c541887
skjw1 <jong.w.kim@tu-berlin.de>
2020-10-04 00:13:23 +0200

    add data_postprocessing.py, fixed train.py

--- train.py
+++ train.py
@@ -16,12 +16,12 @@ class Train(object):
         self.nT = self.env.nT  # ex) dt:0.005 nT = 401
 
         self.epi_path_data, self.epi_term_data = [], []
+        self.epi_path_misc_data, self.epi_term_misc_data = [], []
 
 
     def env_rollout(self):
         for epi in range(self.config.hyperparameters['max_episode']):
-            t0, x0, y0 = self.env.reset()
-            trajectory = np.zeros([1, self.s_dim + self.o_dim + self.a_dim + 2])  # s + o + a + r + ref
+            t, x, y = self.env.reset()
 
             for i in range(self.nT):
                 u = self.path_schedule(epi, i, x, u)
@@ -37,19 +37,13 @@ class Train(object):
                 # PoWER_controller.add_experience(x, u, r, x2, is_term)
                 # gdhp_controller.add_experience(x, u, r, x2, is_term, derivs)
 
+                ref = np.reshape(self.env.scale(self.env.ref_traj(), self.env.ymin, self.env.ymax), [1, -1])
+
                 self.controller.add_experience(x, u, r, x2, is_term)
                 self.controller.train()
 
                 self.epi_path_data.append([x, x2, u])
-                self.epi_path_misc_data.append([r, y2])
-
-                x_record = np.reshape(x, [1, -1])
-                u_record = np.reshape(u, [1, -1])
-                y_record = np.reshape(y2, [1, -1])
-                r_record = np.reshape(r, [1, -1])
-                ref_record = np.reshape(env.scale(env.ref_traj(), env.ymin, env.ymax), [1, -1])
-                step_data = np.concatenate([x_record, u_record, y_record, r_record, ref_record], axis=1)
-                trajectory = np.concatenate([trajectory, step_data], axis=0)
+                self.epi_path_misc_data.append([r, y2, ref, derivs])
 
                 # Proceed loop
                 t, x = t2, x2
@@ -60,25 +54,32 @@ class Train(object):
             self.epi_term_data.append([xT])
             self.epi_term_misc_data.append([rT, yT])
 
+        epi_data = [self.epi_path_data, self.epi_path_misc_data]
+        epi_misc_data = [self.epi_term_data, self.epi_term_misc_data]
+
+        return epi_data, epi_misc_data
+
     def path_schedule(self, epi, i, x, u, epi_path_solution, prev_epi_path_data, training):
         if epi_path_solution is None:  # Initial control
-            u = self.env.initial_control(i, x)
+            u = self.controller.initial_control(epi, i, x, u)
 
         else:  # Exists closed-loop control solution
-            _, _, path_gain_epi, u_epi, l_epi, hypparam_epi, _, _ = epi_path_solution
-            if training == True or prev_epi_path_data == None:
-                # Training phase
-                # x: simulated --> closed-loop solution already computed in the forward-sweep
-                u = u_epi[i]
-                l = l_epi[i]
-            else:
-                # Test phase
-                # x: plant-data --> closed-loop solution need to be computed here
-                x_nom = prev_epi_path_data[i][0]
-                l, Kx, Ks, Kn = path_gain_epi[i]  # [lu, ll]; [Kux, Klx]; [Kus, Kls]; [Kun, Kln]
-                delx = x - x_nom
+            u = self.controller.ctrl(epi, i, x, u)
 
-
-                u = u_epi[i] + delu
+            # _, _, path_gain_epi, u_epi, l_epi, hypparam_epi, _, _ = epi_path_solution
+            # if training == True or prev_epi_path_data == None:
+            #     # Training phase
+            #     # x: simulated --> closed-loop solution already computed in the forward-sweep
+            #     u = u_epi[i]
+            #     l = l_epi[i]
+            # else:
+            #     # Test phase
+            #     # x: plant-data --> closed-loop solution need to be computed here
+            #     x_nom = prev_epi_path_data[i][0]
+            #     l, Kx, Ks, Kn = path_gain_epi[i]  # [lu, ll]; [Kux, Klx]; [Kus, Kls]; [Kun, Kln]
+            #     delx = x - x_nom
+            #
+            #
+            #     u = u_epi[i] + delu
 
         return u
