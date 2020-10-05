e1d33d3fcbd19c93e995cefe8510805c64c35494
skjw1224 <jong.w.kim@tu-berlin.de>
2020-10-05 16:45:43 +0200

    Move hyperparameters to config.py

--- config.py
+++ config.py
@@ -8,20 +8,37 @@ class Config(object):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.environment = None
         self.algorithm = None
-        self.max_episode = None
         self.hyperparameters = None
+        # should contain: 'explore_epi_idx', 'tau', 'max_episode'
 
     def encode_settings(self, **args):
         if 'device' in args: self.device = args['deivce']
         self.environment = args['environment']
         self.algorithm = args['algorithm']
-        self.max_episode = args['max_episode']
+
+        self.hyp_default_settings()
+        # Override alternative values
         self.hyperparameters = args['hyperparameters']
 
+        # Set default algorithm specific settings
         self.alg_default_settings()
 
     def alg_default_settings(self):
         if self.algorithm['controller'] in ['dqn', 'qrdqn']:
             self.algorithm['action_type'] = 'discrete'
             self.algorithm['action_mesh_idx'] = utils.action_meshgen(self.algorithm['single_dim_mesh'], self.environment.a_dim)
+
+    def hyp_default_settings(self):
+        self.hyperparameters['explore_epi_idx'] = 50
+        self.hyperparameters['max_episode'] = 200
+        self.hyperparameters['tau'] = 0.05
+        self.hyperparameters['buffer_size'] = 600
+        self.hyperparameters['minibatch_size'] = 32
+        self.hyperparameters['eps_greedy_denom'] = 1
+        self.hyperparameters['eps_greedy'] = 0.1
+        self.hyperparameters['learning_rate'] = 2E-4
+        self.hyperparameters['adam_eps'] = 1E-4
+        self.hyperparameters['l2_reg'] = 1E-3
+        self.hyperparameters['grad_clip_mag'] = 5.0
+
 
--- dqn.py
+++ dqn.py
@@ -7,20 +7,6 @@ import random
 from replay_buffer import ReplayBuffer
 from ou_noise import OU_Noise
 
-BUFFER_SIZE = 600
-MINIBATCH_SIZE = 32
-TAU = 0.05
-EPSILON = 0.1
-EPI_DENOM = 1.
-
-LEARNING_RATE = 2E-4
-ADAM_EPS = 1E-4
-L2REG = 1E-3
-GRAD_CLIP = 5.0
-
-INITIAL_POLICY_INDEX = 5
-AC_PE_TRAINING_INDEX = 10
-
 class Qnetwork(nn.Module):
     def __init__(self, input_dim, output_dim):
         """
@@ -58,8 +44,19 @@ class DQN(object):
         self.env_a_dim = self.env.a_dim
         self.a_dim = len(self.config.algorithm['action_mesh_idx'][0])
 
+        # hyperparameters
+        self.buffer_size = self.config.hyperparameters['buffer_size']
+        self.minibatch_size = self.config.hyperparameters['minibatch_size']
+        self.learning_rate = self.config.hyperparameters['learning_rate']
+        self.adam_eps = self.config.hyperparameters['adam_eps']
+        self.l2_reg = self.config.hyperparameters['l2_reg']
+        self.eps = self.config.hyperparameters['eps_greedy']
+        self.epi_denom = self.config.hyperparameters['eps_greedy_denom']
+        self.grad_clip_mag = self.config.hyperparameters['grad_clip_mag']
+        self.tau = self.config.hyperparameters['tau']
 
-        self.replay_buffer = ReplayBuffer(self.env, self.device, buffer_size=BUFFER_SIZE, batch_size=MINIBATCH_SIZE)
+
+        self.replay_buffer = ReplayBuffer(self.env, self.device, buffer_size=self.buffer_size, batch_size=self.minibatch_size)
         self.exp_noise = OU_Noise(size=self.env_a_dim)
         self.initial_ctrl = InitialControl(self.env, self.device)
 
@@ -69,11 +66,17 @@ class DQN(object):
         for to_model, from_model in zip(self.target_q_net.parameters(), self.q_net.parameters()):
             to_model.data.copy_(from_model.data.clone())
 
-        self.q_net_opt = torch.optim.Adam(self.q_net.parameters(), lr=LEARNING_RATE, eps=ADAM_EPS, weight_decay=L2REG)
+
+        self.q_net_opt = torch.optim.Adam(self.q_net.parameters(), lr=self.learning_rate, eps=self.adam_eps, weight_decay=self.l2_reg)
 
 
     def ctrl(self, epi, step, x, u):
-        u_idx = self.choose_action(epi, step, x, u)
+        if epi < self.config.hyperparameters['explore_epi_idx']:
+            if step == 0: self.exp_schedule(epi)
+            if np.random.random() <= self.epsilon:
+                u_idx = np.randint(self.a_dim, [1, 1]) # (B, A)
+        else:
+            u_idx = self.choose_action(epi, step, x, u)
         return u_idx
 
     def choose_action(self, epi, step, x, u):
@@ -81,11 +84,6 @@ class DQN(object):
         with torch.no_grad():
             u_idx = self.q_net(x).min(-1)[1].unsqueeze(1) # (B, A)
         self.q_net.train()
-
-        if step == 0: self.exp_schedule(epi)
-        if random.random() <= self.epsilon and epi <= AC_PE_TRAINING_INDEX:
-            u_idx = np.randint(self.a_dim, [1, 1]) # (B, A)
-
         return u_idx
 
     def add_experience(self, *single_expr):
@@ -93,7 +91,8 @@ class DQN(object):
         self.replay_buffer.add(*[x, u_idx, r, x2, term])
 
     def exp_schedule(self, epi):
-        self.epsilon = EPSILON / (1. + (epi / EPI_DENOM))
+
+        self.epsilon = self.eps / (1. + (epi / self.epi_denom))
 
     def train(self):
         s_batch, a_batch, r_batch, s2_batch, term_batch = self.replay_buffer.sample()
@@ -107,13 +106,14 @@ class DQN(object):
 
         self.q_net_opt.zero_grad()
         q_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.q_net.parameters(), GRAD_CLIP)
+        torch.nn.utils.clip_grad_norm_(self.q_net.parameters(), self.grad_clip_mag)
         self.q_net_opt.step()
 
         """Updates the target network in the direction of the local network but by taking a step size
         less than one so the target network's parameter values trail the local networks. This helps stabilise training"""
+
         for to_model, from_model in zip(self.target_q_net.parameters(), self.q_net.parameters()):
-            to_model.data.copy_(TAU * from_model.data + (1 - TAU) * to_model.data)
+            to_model.data.copy_(self.tau * from_model.data + (1 - self.tau) * to_model.data)
 
 class InitialControl(object):
     def __init__(self, env, device):
