5bd275fd93f2246081b900aad7d24d4a1144bfda
J0ONSO0 <jsphero@snu.ac.kr>
2023-10-31 19:34:37 +0900

    Style: Modify main.py using '__name__' variable

--- main.py
+++ main.py
@@ -4,38 +4,39 @@ from env_casadi import CstrEnv
 from config import Config
 from train import Train
 
-config = Config()
-config.environment = CstrEnv()
-config.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-path = 'results/' + config.environment.env_name
-try:
-    os.mkdir('results')
-    os.mkdir(path)
-except FileExistsError:
-    pass
-config.result_save_path = path + '/'
-config.standard_deviation_results = 1.0
-config.save_model = False
 
-alg_settings = {
-    # 'iLQR': None,
-    # 'DQN': None,
-    # 'QRDQN': None,
-    # 'DDPG': None,
-    # 'GDHP': None,
-    # 'SDDP': None,
-    # 'A2C': None,
-    # 'SAC': None,
-    # 'TRPO': None,
-    # 'PPO': None,
-    # 'REPS': None,
-    # 'REPS_NN': None,
-    # 'PoWER': None,
-    # 'PI2': None,
-}
+if __name__ == "__main__":
+    config = Config()
+    config.environment = CstrEnv()
+    config.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    path = 'results/' + config.environment.env_name
+    try:
+        os.mkdir('results')
+        os.mkdir(path)
+    except FileExistsError:
+        pass
+    config.result_save_path = path + '/'
+    config.standard_deviation_results = 1.0
+    config.save_model = False
 
-config.encode_settings(alg_settings)
-trainer = Train(config)
-trainer.env_rollout()
-trainer.plot()
+    alg_settings = {
+        # 'iLQR': None,
+        # 'DQN': None,
+        # 'QRDQN': None,
+        # 'DDPG': None,
+        # 'GDHP': None,
+        # 'SDDP': None,
+        # 'A2C': None,
+        # 'SAC': None,
+        # 'TRPO': None,
+        # 'PPO': None,
+        # 'REPS': None,
+        # 'REPS_NN': None,
+        # 'PoWER': None,
+        # 'PI2': None,
+    }
 
+    config.encode_settings(alg_settings)
+    trainer = Train(config)
+    trainer.env_rollout()
+    trainer.plot()
