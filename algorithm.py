5f806dec7a58e2e8bbee20e1131eed5fbb3d1328
J0ONSO0 <jsphero@snu.ac.kr>
2023-10-31 19:15:44 +0900

    feat: Add base class using abc

---
+++ algorithm.py
@@ -0,0 +1,42 @@
+import abc
+
+
+class Algorithm(object, metaclass=abc.ABCMeta):
+    """
+    Base class for RL algorithms
+    """
+
+    @abc.abstractmethod
+    def ctrl(self, episode, step, state, action):
+        """
+        Return control actions computed from the policy or the initial controller
+        """
+        pass
+
+    @abc.abstractmethod
+    def train(self):
+        """
+        Train agent
+        """
+        pass
+
+    @abc.abstractmethod
+    def add_experience(self, experience):
+        """
+        Add experience to the replay buffer
+        """
+        pass
+
+    @abc.abstractmethod
+    def save(self, path, file_name):
+        """
+        Save the model to the path with file_name
+        """
+        pass
+
+    @abc.abstractmethod
+    def load(self, path, file_name):
+        """
+        Load the model from the path with file_name
+        """
+        pass
