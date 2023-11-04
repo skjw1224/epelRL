f753c2c9c136141dc8461b4fd8379a22a3a20a03
J0ONSO0 <jsphero@snu.ac.kr>
2023-11-04 21:12:08 +0900

    Refactor: Clean up DQN algo

--- algorithm/dqn.py
+++ algorithm/dqn.py
@@ -1,75 +1,61 @@
 import os
 import copy
 import torch
+import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 
-from .algorithm import Algorithm
+from .base_algorithm import Algorithm
+from network.network import CriticMLP
 from replay_buffer.replay_buffer import ReplayBuffer
+from utility.explorers import EpsilonGreedy
 
 
 class DQN(Algorithm):
     def __init__(self, config):
         self.config = config
-        self.env = self.config.environment
         self.device = self.config.device
-
-        self.s_dim = self.env.s_dim
-        self.a_dim = self.env.a_dim
+        self.s_dim = self.config.s_dim
+        self.a_dim = self.config.a_dim
+        self.nT = self.config.nT
 
         # Hyperparameters
-        self.h_nodes = self.config.hyperparameters['hidden_nodes']
-        self.explore_epi_idx = self.config.hyperparameters['explore_epi_idx']
-        self.buffer_size = self.config.hyperparameters['buffer_size']
-        self.minibatch_size = self.config.hyperparameters['minibatch_size']
-        self.learning_rate = self.config.hyperparameters['learning_rate']
-        self.adam_eps = self.config.hyperparameters['adam_eps']
-        self.l2_reg = self.config.hyperparameters['l2_reg']
-        self.grad_clip_mag = self.config.hyperparameters['grad_clip_mag']
-        self.tau = self.config.hyperparameters['tau']
-        self.single_dim_mesh = self.config.hyperparameters['single_dim_mesh']
+        self.num_hidden_nodes = self.config.num_hidden_nodes
+        self.num_hidden_layers = self.config.num_hidden_layers
+        hidden_dim_lst = [self.num_hidden_nodes for _ in range(self.num_hidden_layers)]
 
-        self.explorer = self.config.algorithm['explorer']['function'](config)
-        self.approximator = self.config.algorithm['approximator']['function']
-        self.replay_buffer = ReplayBuffer(self.env, self.device, buffer_size=self.buffer_size, batch_size=self.minibatch_size)
+        self.critic_lr = self.config.critic_lr
+        self.adam_eps = self.config.adam_eps
+        self.l2_reg = self.config.l2_reg
+        self.grad_clip_mag = self.config.grad_clip_mag
+        self.tau = self.config.tau
+        self.single_dim_mesh = self.config.single_dim_mesh
+
+        self.explorer = EpsilonGreedy(config)
+        self.replay_buffer = ReplayBuffer(config)
 
         # Action mesh
-        self.a_mesh_dim, self.a_mesh, self.a_mesh_idx = self.generate_action_mesh()
+        self.a_mesh_dim, self.a_mesh, self.a_mesh_idx = self._generate_action_mesh()
 
         # Critic network (Q network)
-        self.critic = self.approximator(self.s_dim, self.a_mesh_dim, self.h_nodes).to(self.device)  # s --> a
-        self.target_critic = self.approximator(self.s_dim, self.a_mesh_dim, self.h_nodes).to(self.device)  # s --> a
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.learning_rate, eps=self.adam_eps, weight_decay=self.l2_reg)
-
-        for to_model, from_model in zip(self.target_critic.parameters(), self.critic.parameters()):
-            to_model.data.copy_(from_model.data.clone())
+        self.critic = CriticMLP(self.s_dim, self.a_mesh_dim, hidden_dim_lst, F.silu).to(self.device)
+        self.target_critic = CriticMLP(self.s_dim, self.a_mesh_dim, hidden_dim_lst, F.silu).to(self.device)
+        self.target_critic = copy.deepcopy(self.critic)
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=self.critic_lr, eps=self.adam_eps, weight_decay=self.l2_reg)
 
         self.loss_lst = ['Critic loss']
 
-    def ctrl(self, epi, step, s, a):
-        if epi < self.explore_epi_idx:
-            a_idx = self.choose_action(s)
-            a_idx = self.explorer.sample(epi, step, a_idx)
-        else:
-            a_idx = self.choose_action(s)
+    def ctrl(self, state):
+        state = torch.from_numpy(state.T).float().to(self.device)
+        action_idx = self.critic(state).min(-1)[1].unsqueeze(1)
+        action_idx = action_idx.cpu().detach().numpy()
+        action_idx = self.explorer.sample(action_idx)
 
-        return a_idx
+        action = self.idx2action(action_idx)
 
-    def choose_action(self, s):
-        # Numpy to torch
-        s = torch.from_numpy(s.T).float().to(self.device)  # (B, 1)
+        return action
 
-        self.critic.eval()
-        with torch.no_grad():
-            a_idx = self.critic(s).min(-1)[1].unsqueeze(1)
-        self.critic.train()
-
-        # Torch to Numpy
-        a_idx = a_idx.cpu().detach().numpy()
-
-        return a_idx
-
-    def generate_action_mesh(self):
+    def _generate_action_mesh(self):
         num_grid = len(self.single_dim_mesh)
         a_mesh_dim = num_grid ** self.a_dim
         single_dim_mesh = np.array(self.single_dim_mesh)
@@ -78,43 +64,51 @@ class DQN(Algorithm):
 
         return a_mesh_dim, a_mesh, a_mesh_idx
 
-    def action_idx2mesh(self, a_idx):
-        env_a_dim = len(self.a_mesh)
+    def idx2action(self, idx):
+        mesh_idx = np.where(self.a_mesh_idx == idx)
+        action = np.array([self.a_mesh[i][mesh_idx] for i in range(self.a_dim)])
 
-        mesh_idx = (self.a_mesh_idx == a_idx).nonzero()
-        a = np.array([self.a_mesh[i, :][tuple(mesh_idx)] for i in range(env_a_dim)])
+        return action
 
-        return a
+    def action2idx(self, action):
+        action2idx_lst = [self.a_mesh[i] == action[i] for i in range(self.a_dim)]
+        idx_lst = action2idx_lst[0]
+        for i in range(1, len(action2idx_lst)):
+            idx_lst = idx_lst & action2idx_lst[i]
+        idx = np.where(idx_lst)
+        mesh_idx = self.a_mesh_idx[idx].reshape(-1, 1)
+
+        return mesh_idx
 
     def add_experience(self, *single_expr):
-        s, a_idx, r, s2, is_term = single_expr
+        s, a, r, s2, is_term = single_expr
+        a_idx = self.action2idx(a)
+
         self.replay_buffer.add(*[s, a_idx, r, s2, is_term])
 
     def train(self):
-        if len(self.replay_buffer) > 0:
-            s_batch, a_batch, r_batch, s2_batch, term_batch = self.replay_buffer.sample()
+        assert (len(self.replay_buffer) > 0)
 
-            q_batch = self.critic(s_batch).gather(1, a_batch.long())
-            q2_batch = self.target_critic(s2_batch).detach().min(-1)[0].unsqueeze(1) * (1 - term_batch)
-            q_target_batch = r_batch + q2_batch
+        # Replay buffer sample
+        states, actions, rewards, next_states, dones = self.replay_buffer.sample()
 
-            critic_loss = F.mse_loss(q_batch, q_target_batch)
+        # Network update
+        with torch.no_grad():
+            next_q = self.target_critic(next_states).detach().min(-1)[0].unsqueeze(1) * (1 - dones)
+            target_q = rewards + next_q
 
-            self.critic_optimizer.zero_grad()
-            critic_loss.backward()
-            torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self.grad_clip_mag)
-            self.critic_optimizer.step()
+        current_q = self.critic(states).gather(1, actions.long())
+        critic_loss = F.mse_loss(current_q, target_q)
 
-            """Updates the target network in the direction of the local network but by taking a step size
-            less than one so the target network's parameter values trail the local networks. This helps stabilise training"""
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self.grad_clip_mag)
+        self.critic_optimizer.step()
 
-            for to_model, from_model in zip(self.target_critic.parameters(), self.critic.parameters()):
-                to_model.data.copy_(self.tau * from_model.data + (1 - self.tau) * to_model.data)
+        for to_model, from_model in zip(self.target_critic.parameters(), self.critic.parameters()):
+            to_model.data.copy_(self.tau * from_model.data + (1 - self.tau) * to_model.data)
 
-            critic_loss = critic_loss.cpu().detach().numpy().item()
-            loss = np.array([critic_loss])
-        else:
-            loss = np.array([0.])
+        loss = np.array([critic_loss.cpu().detach().numpy().item()])
 
         return loss
 
