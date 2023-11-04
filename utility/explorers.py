3651bc680c2c047999228eeba9fea40774c4a72d
J0ONSO0 <jsphero@snu.ac.kr>
2023-11-04 21:13:00 +0900

    Refactor: Clean up explorers

--- utility/explorers.py
+++ utility/explorers.py
@@ -1,82 +1,97 @@
 import numpy as np
-import random
-import copy
 
-class OU_Noise(object):
+
+class OUNoise(object):
     """Ornstein-Uhlenbeck process."""
     def __init__(self, config):
-        self.config = config
-        self.env = self.config.environment
-        self.a_dim = self.env.a_dim
+        self.a_dim = config.a_dim
+        self.nT = config.nT
 
-        self.mu0 = self.config.hyperparameters['ou_mu0']
-        self.theta = self.config.hyperparameters['ou_theta']
-        self.sigma = self.config.hyperparameters['ou_sigma']
-
-        self.eps0 = self.config.hyperparameters['eps_greedy']
-        self.epi_denom = self.config.hyperparameters['eps_greedy_denom']
+        self.mu0 = 0
+        self.theta = 0.15
+        self.sigma = 0.2
+        self.eps0 = 0.3
+        self.epi_denom = 1
 
         self.mu = self.mu0 * np.ones([self.a_dim, 1])
 
-        random.seed(123)
+        self.step = 0
+        self.episode = 0
+        self.exp_schedule()
 
-    def exp_schedule(self, epi):
+    def exp_schedule(self):
         """Reset the internal state (= noise) to mean (mu)."""
         self.state = self.mu
-        self.epsilon = self.eps0 / (1. + (epi / self.epi_denom))
+        self.epsilon = self.eps0 / (1. + (self.episode / self.epi_denom))
 
-    def sample(self, epi, step, u_nom):
-        if step == 0:
-            self.exp_schedule(epi)
-
+    def sample(self, u_nom):
         """Update internal state and return it as a noise sample."""
         dx = self.theta * (self.mu - self.state) + self.sigma * np.random.randn(self.a_dim, 1)
         self.state += dx
         noise = self.state * self.epsilon
         u_exp = noise + u_nom
 
+        self.step += 1
+        if self.step >= self.nT:
+            self.step = 0
+            self.episode += 1
+            self.exp_schedule()
+
         return u_exp
 
-class E_greedy(object):
+
+class EpsilonGreedy(object):
     def __init__(self, config):
-        self.config = config
-        self.env = self.config.environment
-        self.a_dim = self.env.a_dim
+        self.a_dim = config.a_dim
+        self.mesh_size = len(config.single_dim_mesh) ** self.a_dim
+        self.nT = config.nT
 
-        self.eps0 = self.config.hyperparameters['eps_greedy']
-        self.epi_denom = self.config.hyperparameters['eps_greedy_denom']
+        self.eps0 = 0.3
+        self.epi_denom = 1
 
-    def exp_schedule(self, epi):
-        self.eps = self.eps0 / (1. + (epi / self.epi_denom))
+        self.step = 0
+        self.episode = 0
+        self.exp_schedule()
 
-    def sample(self, epi, step, u_nom):
-        if step == 0:
-            self.exp_schedule(epi)
+    def exp_schedule(self):
+        self.epsilon = self.eps0 / (1. + (self.episode / self.epi_denom))
 
-        if np.random.random() <= self.eps:
-            u_exp = np.random.randint(low=0, high=self.a_dim, size=[1, 1])
+    def sample(self, u_nom):
+        if np.random.random() <= self.epsilon:
+            u_exp = np.random.randint(low=0, high=self.mesh_size, size=[1, 1])
         else:
             u_exp = u_nom
 
+        self.step += 1
+        if self.step >= self.nT:
+            self.step = 0
+            self.episode += 1
+            self.exp_schedule()
+
         return u_exp
 
-class Gaussian_noise(object):
+
+class GaussianNoise(object):
     def __init__(self, config):
-        self.config = config
-        self.env = self.config.environment
-        self.a_dim = self.env.a_dim
+        self.a_dim = config.a_dim
+        self.nT = config.nT
 
-        self.eps_decay_rate = self.config.hyperparameters['eps_decay_rate']
+        self.eps_decay_rate = 0.99
 
-        random.seed(123)
+        self.step = 0
+        self.episode = 0
+        self.exp_schedule()
 
-    def exp_schedule(self, epi):
-        self.eps = self.eps_decay_rate ** epi * np.random.randn(self.a_dim, 1)
+    def exp_schedule(self):
+        self.epsilon = self.eps_decay_rate ** self.episode * np.random.randn(self.a_dim, 1)
 
-    def sample(self, epi, step, u_nom):
-        if step == 0:
-            self.exp_schedule(epi)
+    def sample(self, u_nom):
+        u_exp = self.epsilon + u_nom
 
-        u_exp = self.eps + u_nom
+        self.step += 1
+        if self.step >= self.nT:
+            self.step = 0
+            self.episode += 1
+            self.exp_schedule()
 
         return u_exp
