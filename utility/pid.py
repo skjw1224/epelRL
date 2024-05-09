d200f56f24ccf61fe1cd93f0a8ab105ee2a4e2d7
J0ONSO0 <jsphero@snu.ac.kr>
2024-05-09 17:45:26 +0900

    feat: modify pid class

--- utility/pid.py
+++ utility/pid.py
@@ -1,26 +1,54 @@
-from utility import utils
 import numpy as np
 
+
 class PID(object):
-    def __init__(self, config):
-        self.env = config.environment
+    def __init__(self, pid_info):
+        self.a_dim = pid_info['a_dim']
+        self.o_dim = pid_info['o_dim']
+        self.dt = pid_info['dt']
 
-        self.s_dim = self.env.s_dim
-        self.a_dim = self.env.a_dim
-        self.o_dim = self.env.o_dim
+        self._Kp = np.zeros((self.a_dim, self.o_dim))
+        self._Ki = np.zeros((self.a_dim, self.o_dim))
+        self._Kd = np.zeros((self.a_dim, self.o_dim))
+        
+        self._ref = np.zeros((self.o_dim, 1))
 
-    def ctrl(self, epi, step, x, u):
-        if step == 0:
-            self.ei = np.zeros([self.o_dim, 1])
-        ref = utils.scale(self.env.ref_traj(), self.env.ymin, self.env.ymax)
-        Kp = 2 * np.ones([self.a_dim, self.o_dim])
-        Ki = 0.1 * np.ones([self.a_dim, self.o_dim])
+    def reset(self):
+        self.integral = np.zeros((self.o_dim, 1))
+    
+    @property
+    def get_gain(self):
+        gain = {
+            'Kp': self._Kp,
+            'Ki': self._Ki,
+            'Kd': self._Kd,
+        }
+        return gain
 
-        y = self.env.y_fnc(x, u, self.env.p_mu, self.env.p_sigma, self.env.p_eps).full()
-        u = Kp @ (y - ref) + Ki @ self.ei
+    @get_gain.setter
+    def set_gain(self, gain):
+        self._Kp = gain['Kp']
+        self._Ki = gain['Ki']
+        self._Kd = gain['Kd']
 
-        self.ei = self.ei + (y - ref)
-        return u
+    @property
+    def get_reference(self):
+        return self._ref
 
-    def add_experience(self, *single_expr):
-        pass
+    @get_reference.setter
+    def set_reference(self, ref):
+        self._ref = ref
+
+    def ctrl(self, observ):
+        error = self._ref - observ
+
+        proportional = error
+        integral = self.integral + error * self.dt
+        derivative = (error - self.prev_error) / self.dt
+        
+        action = self._Kp @ proportional + self._Ki @ integral + self._Kd @ derivative
+
+        self.integral = integral
+        self.prev_error = error
+
+        return action
