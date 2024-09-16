8bdc4729ccbc01ae7fcbe2a6d7caad612254c511
skjw1224 <skjw1224@gmail.com>
2024-09-16 11:06:16 +0900

    meta executor

--- meta_executor.py
+++ meta_executor.py
@@ -8,7 +8,7 @@ def main():
     available_envs = [env.__name__ for env in environment.__all__]
     for alg in available_alg:
         for env in available_envs:
-            subprocess.run(['python', 'train_single_env_algo.py', '--algo', alg, '--env', env, '--max_episode', 10, '--save_freq', 2])
+            subprocess.run(['python', 'train_single_env_algo.py', '--algo', alg, '--env', env, '--max_episode', "10", '--save_freq', "2"])
 
 if __name__ == '__main__':
     main()
