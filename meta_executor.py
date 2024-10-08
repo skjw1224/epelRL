ad92d959cf336a53373fc9dd2f8b35e74973afbb
Hyein <976hannah@snu.ac.kr>
2024-10-08 10:34:18 +0900

    Meta executor with the tuned parameters

--- meta_executor.py
+++ meta_executor.py
@@ -3,32 +3,55 @@ import os
 import algorithm
 import environment
 
-def main_tuning():
+def main_TUNING():
     # alg = 'QRDQN'
-    env = 'CSTR'
-    available_algs = ['QRDQN', 'TRPO']
-    lr_algs = [0.0001, 0.0001]
-    value_lst = [0.1, 0.01, 0.0001, 0.00001]
+    env = 'POLYMER'
+    available_algs = ['DQN']
 
     for idx, alg in enumerate(available_algs):
-        for v in value_lst:
-            lr = lr_algs[idx]
-            subprocess.run(['python', 'train_single_env_algo.py', '--algo', alg, '--env', env,
-                            '--max_episode', "100", '--save_freq', "20", '--warm_up_episode', '1',
-                            '--convg_bound', '0.01', '--critic_lr', str(lr), '--l2_reg', str(v)])
+        subprocess.run(['python', 'train_single_env_algo.py', '--algo', alg, '--env', env,
+                        '--max_episode', "10", '--save_freq', "2", '--warm_up_episode', '1',
+                        '--convg_bound', '0.01', '--rbf_dim', '100', '--rbf_type', 'matern32'])
+        # subprocess.run(['python', 'test_single_env_algo.py', '--algo', alg, '--env', env])
+
+    # subprocess.run(['python', 'train_single_env_algo.py', '--algo', 'REPS', '--env', env,
+    #                 '--max_episode', "10", '--save_freq', "2", '--warm_up_episode', '1'])
+    # subprocess.run(['python', 'test_single_env_algo.py', '--algo', 'REPS', '--env', env])
 
 
 def main():
     # available_algs = [alg.__name__ for alg in algorithm.__all__]
     # available_envs = [env.__name__ for env in environment.__all__]
 
-    available_algs = ['A2C', 'DDPG']
-    available_envs = ['CSTR']
+    available_algs = ['TRPO']
+    # available_algs = ['A2C', 'DDPG', 'DQN',
+    #                   # 'iLQR', 'GDHP', 'SDDP',
+    #                   'QRDQN', 'SAC', 'TD3',
+    #                   'PPO', 'TRPO']
+    #                   # 'PI2', 'PoWER', 'REPS']
+    available_envs = ['POLYMER']  # POLYMER, 'CRYSTAL', 'DISTILLATION'
+    # (critic_lr, adam_eps, l2_reg)
+    hyp_params = {
+        'A2C': (0.01, 1.e-6, 1.e-3),
+        'DDPG': (0.001, 0.0001, 1.e-3),
+        'DQN': (0.01, 1.e-6, 1.e-3),
+        'iLQR': (1.e-4, 1.e-6, 1.e-3),
+        'GDHP': (1.e-4, 1.e-6, 1.e-3),
+        'SDDP': (1e-4, 1e-6, 1e-3),
+        'QRDQN': (0.0001, 1.e-6, 0.1),
+        'SAC': (1e-4, 1e-6, 1e-3),
+        'TD3': (0.01, 1.e-6, 1.e-3),
+        'PPO': (0.001, 1.e-6, 1.e-3),
+        'TRPO': (1e-4, 1e-6, 1e-3),
+        'PoWER': (1.e-4, 1.e-6, 1.e-3, 'Matern32'),
+        'REPS': (1.e-4, 1.e-6, 1.e-3, 'Gaussian')
+    }
     for env in available_envs:
-        for alg in available_algs:
+        for idx, alg in enumerate(available_algs):
+            lr, eps, l2reg = hyp_params[alg]
             subprocess.run(['python', 'train_single_env_algo.py', '--algo', alg, '--env', env,
-                            '--max_episode', "5", '--save_freq', "1", '--warm_up_episode', '1',
-                            '--convg_bound', '2.5e-2'])
+                            '--max_episode', "1000", '--save_freq', "100", '--warm_up_episode', '1',
+                            '--convg_bound', '0.1', '--critic_lr', str(lr), '--adam_eps', str(eps), '--l2_reg', str(l2reg)])
             subprocess.run(['python', 'test_single_env_algo.py', '--algo', alg, '--env', env])
 
         subprocess.run(['python', 'test_plot.py', '--env', env])
