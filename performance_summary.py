48ff1ea0a50e1daae7d99f85915567bb837bb443
Hyein <976hannah@snu.ac.kr>
2024-10-24 16:55:13 +0900

    Plot radar chart comparing regulation vs. maximization envs

--- performance_summary.py
+++ performance_summary.py
@@ -5,8 +5,31 @@ import matplotlib.pyplot as plt
 import glob
 
 import algorithm
+from utility.plotting import plot_radar
 
-def get_summary_history(available_algs, available_envs, summary_path):
+alg_groups = {'Value-based method': ['DQN', 'QRDQN'],
+              'Policy-based method': ['A2C', 'TRPO', 'PPO', 'DDPG', 'TD3'],
+              'Inference-based method': ['PoWER', 'REPS', 'PI2', 'SAC'],
+              'Model-based method': ['iLQR', 'SDDP', 'GDHP']}
+
+def get_summary_history():
+    # available_algs = [alg.__name__ for alg in algorithm.__all__]
+    # available_envs = [env.__name__ for env in environment.__all__]
+    # available_algs = ['A2C', 'DDPG', 'DQN', 'iLQR', 'PPO', 'QRDQN', 'SAC', 'TD3', 'SDDP']
+
+    available_envs = ['POLYMER', 'PFR', 'DISTILLATION', 'CSTR', 'CRYSTAL']
+    env_name = available_envs[0]
+    available_file_path = glob.glob(f'./_Result/test_{env_name}_*')
+    available_algs = []
+    for dir_path in available_file_path:
+        dir_name = os.path.basename(dir_path)
+        if os.path.isfile(os.path.join(dir_path, 'test_traj_data_history.npy')):
+            alg_name = dir_name[6 + len(env_name):]
+            available_algs.append(alg_name)
+    print(available_algs)
+    summary_path = os.path.join(f'./_Result', 'summary')
+    os.makedirs(summary_path, exist_ok=True)
+
     history = []
     summary_feature = ['Episodic Computation', 'Converged Epi',
                        'Convergence Criteria',
@@ -46,27 +69,10 @@ def get_summary_history(available_algs, available_envs, summary_path):
                               index=available_envs+['Average', 'Minus Err', 'Plus Err', 'Median'])
         alg_df.to_csv(os.path.join(summary_path, f'table_{alg_name}.csv'))
 
-    return summary_feature, history
+    return available_algs, available_envs, summary_path, summary_feature, history
 
-def performance_summary():
-    # available_algs = [alg.__name__ for alg in algorithm.__all__]
-    # available_envs = [env.__name__ for env in environment.__all__]
-    # available_algs = ['A2C', 'DDPG', 'DQN', 'iLQR', 'PPO', 'QRDQN', 'SAC', 'TD3', 'SDDP']
-
-    available_envs = ['CRYSTAL', 'PFR', 'DISTILLATION', 'CSTR', 'POLYMER']
-    env_name = available_envs[0]
-    available_file_path = glob.glob(f'./_Result/test_{env_name}_*')
-    available_algs = []
-    for dir_path in available_file_path:
-        dir_name = os.path.basename(dir_path)
-        if os.path.isfile(os.path.join(dir_path, 'test_traj_data_history.npy')):
-            alg_name = dir_name[6 + len(env_name):]
-            available_algs.append(alg_name)
-    print(available_algs)
-    summary_path = os.path.join(f'./_Result', 'summary')
-    os.makedirs(summary_path, exist_ok=True)
-
-    summary_feature, history = get_summary_history(available_algs, available_envs, summary_path)
+def plot_per_algorithm():
+    available_algs, available_envs, summary_path, summary_feature, history = get_summary_history()
 
     # Bar Chart
     bar_width = 0.25
@@ -99,102 +105,70 @@ def performance_summary():
                       "max": [max([h[f][0] for h in history]) for f in range(len(summary_feature))]}
     scaling_factor["scale"] = [max(scaling_factor["max"][f] - scaling_factor["min"][f], 0.01)for f in range(len(summary_feature))]
 
-    cat = summary_feature
-    cat = [*cat, cat[0]]
     radar_data = {}
-
-    label_loc = np.linspace(start=0, stop=2 * np.pi, num=len(cat))
-
     for i, alg_name in enumerate(available_algs):
-        ax_alg = plt.subplot(polar=True)
-
         alg_data = [(history[i][f][0] - scaling_factor["min"][f]) / scaling_factor["scale"][f]
                     for f in range(len(summary_feature))]
         alg_data = [*alg_data, alg_data[0]]
         radar_data[alg_name] = alg_data
-
-        ax_alg.plot(label_loc, alg_data, 'o--', color='#468dce')
-        ax_alg.fill(label_loc, alg_data, alpha=0.15, color='#468dce')
-        ax_alg.set_theta_offset(np.pi / 2)
-        ax_alg.set_theta_direction(-1)
-        ax_alg.set_thetagrids(np.degrees(label_loc), cat)
-
-        for label, angle in zip(ax_alg.get_xticklabels(), label_loc):
-            label.set_rotation(angle * 180. / np.pi - 90.)
-            if 0 < angle < np.pi:
-                label.set_horizontalalignment('left')
-            else:
-                label.set_horizontalalignment('right')
-            label.set_rotation_mode("anchor")
-
-        # Ensure radar goes from 0 to 100. it also removes the extra line
-        ax_alg.set_ylim(0, 1.01)
-        # You can also set gridlines manually like this:
-        ax_alg.set_rgrids([.2, .4, .6, .8, 1.])
-        ax_alg.invert_yaxis()
-
-        # Set position of y-labels (0-100) to be in the middle
-        # of the first two axes.
-        # ax.set_rlabel_position(180 / n_points)
-
-        # Make the y-axis (0-100) labels smaller.
-        ax_alg.tick_params(axis='y', labelsize=8)
-        # Change the color of the circular gridlines.
-        ax_alg.grid(color='#AAAAAA')
-        # Change the color of the outermost gridline (the spine).
-        ax_alg.spines['polar'].set_color('#eaeaea')
-        # Change the background color inside the circle itself.
-        ax_alg.set_facecolor('#FAFAFA')
-
-        # Add title.
-        ax_alg.set_title(alg_name, y=1.08)
-
-        # Add a legend as well.
-        # ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
-
-        plt.savefig(os.path.join(summary_path, f'{alg_name}.png'))
-        plt.show()
-        plt.close()
+        alg_filename = os.path.join(summary_path, f'{alg_name}.png')
+        plot_radar([alg_data], [alg_name], summary_feature, alg_filename, alg_name)
 
     # Radar Chart per Group
-    alg_groups = {'Value-based method': ['DQN', 'QRDQN'],
-                  'Policy-based method': ['A2C', 'TRPO', 'PPO', 'DDPG', 'TD3'],
-                  'Inference-based method': ['PoWER', 'REPS', 'PI2', 'SAC'],
-                  'Model-based method': ['iLQR', 'SDDP', 'GDHP']}
-    colors = ['#468dce', '#ffd044', '#d54141', '#ff8497', '#cb2fed']
-
     for key, val in alg_groups.items():
-        ax_group = plt.subplot(polar=True)
+        group_data = []
+        valid_algo = []
         for i, alg_name in enumerate(val):
             if alg_name in radar_data.keys():
-                alg_data = radar_data[alg_name]
-                ax_group.plot(label_loc, alg_data, 'o--', color=colors[i], label=alg_name)
-                ax_group.fill(label_loc, alg_data, alpha=0.15, color=colors[i])
-        ax_group.set_theta_offset(np.pi / 2)
-        ax_group.set_theta_direction(-1)
-        ax_group.set_thetagrids(np.degrees(label_loc), cat)
+                group_data.append(radar_data[alg_name])
+                valid_algo.append(alg_name)
+        filename = os.path.join(summary_path, f'radar_{key}.png')
+        plot_radar(group_data, valid_algo, summary_feature, filename, key)
 
-        for label, angle in zip(ax_group.get_xticklabels(), label_loc):
-            if 0 < angle < np.pi:
-                label.set_horizontalalignment('left')
-            else:
-                label.set_horizontalalignment('right')
+def plot_per_env():
+    available_algs, available_envs, summary_path, summary_feature, _ = get_summary_history()
 
-        ax_group.set_ylim(0, 1.01)
-        ax_group.set_rgrids([.2, .4, .6, .8, 1.])
-        ax_group.invert_yaxis()
+    env_groups = {'Regulation': ['CSTR', 'DISTILLATION', 'PFR'],
+                 'Maximization': ['CRYSTAL', 'PENICILLIN', 'POLYMER']}
+    alg_summary = {}
+    for alg_name in available_algs:
+        alg_summary[alg_name] = {}
+        alg_df = pd.read_csv(os.path.join(summary_path, f'table_{alg_name}.csv'))
+        for env_group_name, env_group_elements in env_groups.items():
+            env_group_data = []
+            for env in env_group_elements:
+                env_data = alg_df[alg_df['Unnamed: 0'] == env].to_numpy()
+                if len(env_data)>0:
+                    env_group_data.append(env_data[:,1:].reshape([-1]))
+            alg_summary[alg_name][env_group_name] = np.average(np.array(env_group_data), axis=0).tolist()
+    scaling_factor = {"min": {}, "max": {}, "scale": {}}
+    for env_group_name in env_groups.keys():
+        scaling_factor["min"][env_group_name] = [min([h[env_group_name][f] for h in alg_summary.values()])
+                                                 for f in range(len(summary_feature))]
+        scaling_factor["max"][env_group_name] = [max([h[env_group_name][f] for h in alg_summary.values()])
+                                                 for f in range(len(summary_feature))]
+        scaling_factor["scale"][env_group_name] = \
+            [max(scaling_factor["max"][env_group_name][f] - scaling_factor["min"][env_group_name][f], 0.01)
+             for f in range(len(summary_feature))]
 
-        ax_group.tick_params(axis='y', labelsize=8)
-        ax_group.grid(color='#AAAAAA')
-        ax_group.spines['polar'].set_color('#eaeaea')
-        ax_group.set_facecolor('#FAFAFA')
+    for alg_group_name, alg_group_elements in alg_groups.items():
+        group_data = []
+        for alg in alg_group_elements:
+            if alg in alg_summary.keys():
+                group_data.append(alg_summary[alg])
+        env_group_data = []
+        env_group_label = []
+        for env_group_idx, env_group_name in enumerate(env_groups.keys()):
+            avg_val = np.average(np.array([g[env_group_name] for g in group_data]), axis=0).tolist()
+            avg_val_scaled = [(avg_val[i] - scaling_factor["min"][env_group_name][i]) / scaling_factor["scale"][env_group_name][i] for i in range(len(summary_feature))]
+            env_group_data.append([*avg_val_scaled, avg_val_scaled[0]])
+            env_group_label.append(env_group_name)
+        filename = os.path.join(summary_path, f'env_radar_{alg_group_name}.png')
 
-        ax_group.set_title(key, y=1.08)
-        ax_group.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
+        plot_radar(env_group_data, env_group_label, summary_feature, filename, alg_group_name)
 
-        plt.savefig(os.path.join(summary_path, f'radar_{key}.png'))
-        plt.show()
-        plt.close()
+
 
 if __name__ == '__main__':
-    performance_summary()
+    # plot_per_algorithm()
+    plot_per_env()
---
+++ utility/plotting.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plot_radar(data, label, feature, filename, title=''):
+    """ data = (N_label, N_feature) """
+    cat = feature
+    cat = [*cat, cat[0]]
+
+    ax = plt.subplot(polar=True)
+    cat_loc = np.linspace(start=0, stop=2 * np.pi, num=len(cat))
+
+    colors = ['#468dce', '#ffd044', '#d54141', '#ff8497', '#cb2fed']
+    for idx, d in enumerate(data):
+        ax.plot(cat_loc, d, 'o--', color=colors[idx], label=label[idx])
+        ax.fill(cat_loc, d, alpha=0.15, color=colors[idx])
+
+    ax.set_theta_offset(np.pi / 2)
+    ax.set_theta_direction(-1)
+    ax.set_thetagrids(np.degrees(cat_loc), cat)
+
+    for xtick, angle in zip(ax.get_xticklabels(), cat_loc):
+        if 0 < angle < np.pi:
+            xtick.set_horizontalalignment('left')
+        else:
+            xtick.set_horizontalalignment('right')
+
+    ax.set_ylim(0, 1.01)
+    ax.set_rgrids([.2, .4, .6, .8, 1.])
+    ax.invert_yaxis()
+
+    ax.tick_params(axis='y', labelsize=8)
+    ax.grid(color='#AAAAAA')
+    ax.spines['polar'].set_color('#eaeaea')
+    ax.set_facecolor('#FAFAFA')
+
+    ax.set_title(title, y=1.08)
+    if len(data) > 1:
+        ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
+
+    plt.savefig(filename)
+    plt.show()
+    plt.close()
